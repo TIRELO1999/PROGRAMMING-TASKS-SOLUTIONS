3c0eedd535b08864eafde546ca850c36d8ec31a2
TIRELO LESUFI <50773669+TIRELO1999@users.noreply.github.com>
2021-02-08 14:20:27 +0200

    Update week1_RandomNumbers.py

--- week1_RandomNumbers.py
+++ week1_RandomNumbers.py
@@ -6,7 +6,7 @@ as many times as they want. The program should also let the user know
 whether they must guess higher or lower when they are wrong, appropriate
 message should be printed whe wrong/right.
 
-NOTE : YOU CAN USE 000 TO TERMINATE THE PROGRAM
+NOTE : YOU CAN USE 0 TO TERMINATE THE PROGRAM
 
 ========== CODING WITH CLIFFORD ==========
 
@@ -16,7 +16,7 @@ import random
 random_num=random.randint(1,9) # randomly assigns the number from (1,9) to random_num
 guess_num=int(input("Enter the number you chose (1-9) :")) # reads the guessed number from the user
 
-while guess_num!="000":
+while guess_num!=0:
     if guess_num==random_num: # executes when the random number is correctly guessed
         print("********** You guessed the number **********")
         break
